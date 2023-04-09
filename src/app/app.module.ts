1c832520d6baf6e6243d0eafcc2dc93fb6bb7889
Giwa Uthman <ugiwa@eminenttechnology.com>
2023-04-09 15:44:19 +0100

    Add bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,13 @@ import { AppComponent } from './app.component';
 import { CustomerModule } from './customer/customer.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LoginComponent } from './components/login/login.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { RegisterComponent } from './components/register/register.component';
 import { HttpClientModule } from '@angular/common/http';
 import { SharedModule } from './components/shared/shared/shared.module';
 import { LayoutModule } from '@angular/cdk/layout';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -30,6 +31,7 @@ import { LayoutModule } from '@angular/cdk/layout';
     FormsModule,
     ReactiveFormsModule,
     FlexLayoutModule, 
+    NgbModule,
     
     //feature modules
     AdminModule,

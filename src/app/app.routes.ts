dd8eb9c4ce50bf754cb508629e19f291742fba21
Ayomiidey <quadriayomiidey@gmail.com>
2024-06-15 17:00:29 +0100

    WIP

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { LayoutComponent } from './layout/layout.component';
 
-export const routes: Routes = [];
+export const appRoutes: Routes = [
+  // Landing routes
+  {
+    path: '',
+    component: LayoutComponent,
+    data: {
+      layout: 'empty',
+    },
+    children: [
+      {
+        path: 'home',
+        loadChildren: () => import('./modules/landing/home/home.routes'),
+      },
+    ],
+  },
+];
